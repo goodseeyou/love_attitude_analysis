3a85473e522a24bdba5e9a9f1f7308f6554148c9
paul <good_see_you@hotmail.com>
2013-09-07 22:41:04 +0800

    add loading gif

--- index.php
+++ index.php
@@ -5,13 +5,17 @@
 </head>
 <script src="jquery-1.10.1.min.js"></script>
 <body>
+<div class="menu" id="loveAttitude"><a href="javascript:loadExam()">愛情態度量表</a></div>
 <div class="exam"></div>
 <script>
-$.get("love_attitude_exam.php",function(data){
-	$(".exam").css("display","none");
-	$(".exam").html(data);
-	$(".exam").fadeIn(300);
-});
+function loadExam(){
+	$(".exam").html('<img src="loading.gif"/>');
+	$.get("love_attitude_exam.php",function(data){
+		$(".exam").css("display","none");
+		$(".exam").html(data);
+		$(".exam").fadeIn(300);
+	});
+}
 </script>
 </body>
 </html>
